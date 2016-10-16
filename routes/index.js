0af4ea0de6ca62b70496eddf1189900b752cd0f0
sdarmaputra <surya.igede@gmail.com>
2016-10-16 20:51:53 +0700

    change template from jade to pug and create test page for react testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var React = require('react');
+var ReactServer = require('react-dom/server');
+var Sidebar = require('../builds/components/sidebar.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/test', function(req, res, next) {
+	var body = React.createElement(Sidebar);
+	var html = ReactServer.renderToString(body);
+	res.render('tests', { title: 'Express', bodyContent: html });
 });
 
 module.exports = router;
