05c52b96e167a5be6375679ed239d3426297986a
sdarmaputra <surya.igede@gmail.com>
2016-11-05 18:23:39 +0700

    clearing luthfie's preview

--- app/utils/component-previews/luthfie.js
+++ app/utils/component-previews/luthfie.js
@@ -1,6 +1,5 @@
 var components = {
-	container2: { file: '../components/Container.jsx' },
-	registration2: { file: '../components/sidebar/registration/Wrapper.jsx' },
+	
 }
 
 module.exports = components;
